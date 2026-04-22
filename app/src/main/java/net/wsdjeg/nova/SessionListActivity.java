d52da1731e78b8b4b2b172e60cb82945799238c5
Eric Wong <wsdjeg@outlook.com>
2026-04-22 16:45:23 +0800

    feat: 将菜单改为白色三竖点图标下拉菜单
    - 会话列表页面右上角改为白色三竖点，点击弹出"设置"、"关于"
    - Chat页面右上角改为白色三竖点，点击弹出"刷新"、"预览"、"设置"
    - 添加 ic_more_vert_white.xml 图标资源

--- app/src/main/java/net/wsdjeg/nova/SessionListActivity.java
+++ app/src/main/java/net/wsdjeg/nova/SessionListActivity.java
@@ -104,6 +104,9 @@ public class SessionListActivity extends AppCompatActivity implements SessionAda
             // 从设置页面返回后会自动调用 onResume 刷新
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
+        } else if (id == R.id.action_about) {
+            startActivity(new Intent(this, AboutActivity.class));
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
