163b738028814fb0a3a3ce668e424bafe164947a
Eric Wong <wsdjeg@outlook.com>
2026-04-27 23:42:13 +0800

    fix: 修复 ChatActivity.java 编译错误（补全缺失的括号）

--- app/src/main/java/net/wsdjeg/nova/ChatActivity.java
+++ app/src/main/java/net/wsdjeg/nova/ChatActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
@@ -387,6 +389,7 @@ public class ChatActivity extends AppCompatActivity {
                     if (wasAtBottom) forceScrollToBottom();
                     
                     // 会话预览只由 /sessions API 返回，不再在此更新
+                });
             }
             
             @Override
