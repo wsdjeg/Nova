fb8012a338b13062c9ddfd310a4b9078c924bbd8
Eric Wong <wsdjeg@outlook.com>
2026-08-16 00:14:45 +0800

    fix: add missing NonNull import in LogViewerActivity

--- app/src/main/java/net/wsdjeg/nova/LogViewerActivity.java
+++ app/src/main/java/net/wsdjeg/nova/LogViewerActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
